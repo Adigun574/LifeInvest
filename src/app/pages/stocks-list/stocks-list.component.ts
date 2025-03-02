1755a11ba080f738e7eb511d0bad3ad16e0e0dac
adigun574 <adigunibrahim574@gmail.com>
2025-03-02 12:16:53 +0100

    setup routing

---
+++ src/app/pages/stocks-list/stocks-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-stocks-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './stocks-list.component.html',
+  styleUrl: './stocks-list.component.scss'
+})
+export class StocksListComponent {
+
+}
